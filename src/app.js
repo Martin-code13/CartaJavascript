2d9743cdd0e3b27e3330ff10ac1e899781123581
Martin-code13 <mstelo234@gmail.com>
2023-01-04 18:27:47 +0000

    Mi primer commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,33 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let palos = ["♥", "♠", "♣", "♦"];
+  let indexNumbers = numbers[Math.floor(Math.random() * numbers.length)];
+  console.log(indexNumbers);
+
+  let indexpalos = palos[Math.floor(Math.random() * palos.length)];
+  document.querySelector(".top").innerHTML = indexpalos;
+  document.querySelector(".bottom").innerHTML = indexpalos;
+  document.querySelector(".number").innerHTML = indexNumbers;
+  if (indexpalos == "♥" || indexpalos == "♦") {
+    document.querySelector(".top").style.color = "red";
+    document.querySelector(".bottom").style.color = "red";
+    document.querySelector(".number").style.color = "red";
+  }
 };
